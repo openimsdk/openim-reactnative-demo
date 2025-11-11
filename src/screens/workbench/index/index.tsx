ea3852cc61529b1dd17fa3f8cc75ca1e31a30d03
chan <1499188352@qq.com>
2025-11-11 17:19:07 +0800

    feat: enhance Workbench screen

--- src/screens/workbench/index/index.tsx
+++ src/screens/workbench/index/index.tsx
@@ -1,9 +1,24 @@
-import { Text, View } from "react-native";
+import { StatusBar, Text, View } from "react-native";
+import { styles } from "./styles";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import WebView from "react-native-webview";
 
 export default function WorkbenchScreen() {
   return (
-    <View>
-      <Text>Workbench</Text>
+    <View style={styles.container}>
+      <WorkbenchHeader />
+      <WebView source={{ uri: 'https://docs.openim.io/' }} style={{ flex: 1 }}/>
+    </View>
+  );
+}
+
+function WorkbenchHeader() {
+  const insets = useSafeAreaInsets();
+
+  return (
+    <View style={[styles.header, { paddingTop: insets.top }]}>
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent={true}/>
+      <Text style={styles.headerTitle}>工作台</Text>
     </View>
   );
 }
---
+++ src/screens/workbench/index/styles.ts
@@ -0,0 +1,18 @@
+import { theme } from "@/styles/theme";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    backgroundColor: '#fff',
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+  },
+  headerTitle: {
+    fontSize: 20,
+    color: theme.colors.fontBlack,
+    fontWeight: 'bold',
+  },
+});
